264f71f84a995cb23e2ce902a93d3f22f5956942
davidliem99 <davidclerion@gmail.com>
2019-01-28 11:46:56 +0700

    ujian backend jc7

--- index.js
+++ index.js
@@ -1,134 +1,139 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mysql = require('mysql');
 
 var port = 2000;
+
 var app = express({defaultErrorHandler:false});
 app.use(cors())
-
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-const mysql = require('mysql')
 
 const conn = mysql.createConnection({
     host:'localhost',
     user: 'root',
     password:'',
-    database:'maxx-corner',
+    database:'moviebertasbih',
     port:3306 
 });
+
 app.get('/', (req,res)=>{
-    res.send("<h1>On Going</h1>")
+    res.send("<center><h1>Ini Homepage</h1></center>")
 })
 
-//user
+//movies
 
-//ambil data array user
-app.get('/users' , (req,res) => {
-    var sql = 'select * from users;'
+//ambil data movies
+app.get('/movies' , (req,res) => {
+    var sql = 'select * from movies;'
     conn.query(sql ,(err,result)=>{
         res.send(result)
     })
 })
-//menambahkan akun baru
-app.post('/register' , (req,res) => {
-    var newUsers = req.body
-    var sql =`insert into users set ?`
-    conn.query(sql ,newUsers,(err,result)=>{
+//add new movie
+app.post('/add-movie' , (req,res) => {
+    var newMovie = req.body
+    var sql =`insert into movies set ?`
+    conn.query(sql ,newMovie,(err,result)=>{
         console.log(result)
         res.send(result)
     })
 })
-//edit user
-app.post('/edit-user/:id', (req,res)=>{
-    var editUser = req.body
-    var sql = `update users set ? where id = ${req.params.id}`
-    conn.query(sql, editUser,(err,result)=>{
+//edit movie
+app.post('/edit-movie/:id', (req,res)=>{
+    var editMovie = req.body
+    var sql = `update movies set ? where id_mov = ${req.params.id}`
+    conn.query(sql, editMovie,(err,result)=>{
         res.send(result)
     })
 })
-//delete user
-app.post('/delete-user/:id', (req,res)=>{
-    var sql = `delete from users where id = ${req.params.id}`
+//delete movie
+app.post('/delete-movie/:id', (req,res)=>{
+    var sql = `delete from movies where id_mov = ${req.params.id}`
     conn.query(sql, (err,result)=>{
         res.send(result)
     })
 })
-//akhir user
+//end movies
 
+//-------------------------------------------------------------------------------------------------------------->
 
-
-//ambil data produk
-app.get('/produk' , (req,res) => {
-    var sql = 'select * from produk;'
-    conn.query(sql ,(err,result)=>{
-        console.log(result)
-        res.send(result)
-    })
-})
-
-//menambahkan produk baru
-app.post('/insert-produk' , (req,res) => {
-    var newProduct = req.body
-    var sql =`insert into produk set ?`
-    conn.query(sql ,newProduct,(err,result)=>{
-        console.log(result)
-        res.send(result)
-    })
-})
-
-//edit produk
-app.post('/edit-produk/:id',(req,res)=>{
-    var editProduk =req.body
-    var sql = `update produk set ? where id_prod=${req.params.id}`
-    conn.query(sql, editProduk,(err,result)=>{
-        res.send(result)
-    })
-})
-//delete produk
-app.post('/delete-produk/:id',(req,res)=>{
-    var sql = `delete from produk where id_prod = ${req.params.id}`
-    conn.query(sql, (err,result)=>{
-        res.send(result)
-    })
-})
-
-//menambahkan data cart
-app.post('/to-cart' , (req,res) => {
-    var newCart = req.body
-    var sql =`insert into cart set ?`
-    conn.query(sql ,newCart,(err,result)=>{
-        console.log(result)
-        res.send(result)
-    })
-})
-//update atau edit cart
-app.post('/edit-cart/:id' ,(req,res)=>{
-    var newChart =req.body
-    var sql = `update cart set ? where idcart= ${req.params.id}`
-    conn.query(sql, newChart ,(err,result)=>{
-        res.send(result)
-        console.log(result)
-    })
-})
-// delete cart
-app.post('/delete-cart/:id' ,(req,res)=>{
-    var sql = `delete from cart where idcart = ${req.params.id}`
-    conn.query(sql, (err,result)=>{
-        res.send(result)
-        console.log(result)
-    })
-})
-//ambil data many to many untuk cart
-app.get('/cart' , (req,res) => {
-    var sql = 'select us.username as username, pr.namaprod as nama, pr.hargaprod as harga from cart ch join users us on ch.id_user = us.id join produk pr on ch.id_prod = pr.idprod '
+//ambil data categories
+app.get('/categories' , (req,res) => {
+    var sql = 'select * from categories;'
     conn.query(sql ,(err,result)=>{
         console.log(result)
         res.send(result)
     })
 })
 
+//add new categories
+app.post('/add-cat' , (req,res) => {
+    var newCategories = req.body
+    var sql =`insert into categories set ?`
+    conn.query(sql ,newCategories,(err,result)=>{
+        console.log(result)
+        res.send(result)
+    })
+})
 
-app.listen(port, () => console.log('API Aktif di port ' + port))
+//edit categories
+app.post('/edit-cat/:id',(req,res)=>{
+    var editCategories =req.body
+    var sql = `update categories set ? where id_cat=${req.params.id}`
+    conn.query(sql, editCategories,(err,result)=>{
+        res.send(result)
+    })
+})
+//delete categories
+app.post('/delete-cat/:id',(req,res)=>{
+    var sql = `delete from categories where id_cat = ${req.params.id}`
+    conn.query(sql, (err,result)=>{
+        res.send(result)
+    })
+})
+//end categories
+
+//-------------------------------------------------------------------------------------------------------------->
+
+//menambahkan data connect movie & categories
+app.post('/addmovcat' , (req,res) => {
+    var newMC = req.body
+    var sql =`insert into movcat set ?`
+    conn.query(sql ,newMC,(err,result)=>{
+        console.log(result)
+        res.send(result)
+    })
+})
+//update atau edit data movcat
+app.post('/edit-movcat/:id' ,(req,res)=>{
+    var editMC =req.body
+    var sql = `update movcat set ? where id_mov= ${req.params.id}`
+    conn.query(sql, editMC ,(err,result)=>{
+        res.send(result)
+        console.log(result)
+    })
+})
+// delete movcat
+app.post('/delete-movcat/:id' ,(req,res)=>{
+    var sql = `delete from movcat where id_mov = ${req.params.id}`
+    conn.query(sql, (err,result)=>{
+        res.send(result)
+        console.log(result)
+    })
+})
+//ambil data many to many untuk movcat
+app.get('/movcat' , (req,res) => {
+    var sql = 'select nm.nama as nama, ct.namacat as namacat from movcat mc join movies nm on mc.id_mov = nm.id_mov join categories ct on mc.id_cat = ct.id_cat '
+    conn.query(sql ,(err,result)=>{
+        console.log(result)
+        res.send(result)
+    })
+})
+
+//-------------------------------------------------------------------------------------------------------------->
+
+app.listen(port, () => console.log('API Running On port ' + port))
